098b7785ad5a183bcd64498a39a1ba1a75293499
azu-any <azuany.mila@gmail.com>
2026-04-21 12:18:46 -0600

    reprogramar reserva

--- server/src/routes/reservas.ts
+++ server/src/routes/reservas.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/auth';
-import { cancelarReserva, crearReserva, generarQrCodeInvitacion, generarQrCodeAcceso } from '../controllers/reservasController'
+import { cancelarReserva, crearReserva, generarQrCodeInvitacion, generarQrCodeAcceso, reprogramarReserva } from '../controllers/reservasController'
 
 const router = Router();
 
@@ -8,6 +8,7 @@ const router = Router();
 router.use(authenticate);
 
 router.patch('/:reservaId/cancel', cancelarReserva)
+router.put('/:reservaId/reschedule', reprogramarReserva)
 router.get('/:reservaId/qrCode/invite', generarQrCodeInvitacion)
 router.get('/:reservaId/qrCode/acceso', generarQrCodeAcceso)
 
